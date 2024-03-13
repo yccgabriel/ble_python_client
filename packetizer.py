af3fae281d0a0f455e1c0924770e6047c8e825bc
Gabriel Y C Chan <isoleucine.local>
2024-03-13 13:24:12 +0800

    - add crc - pass all tests

--- packetizer.py
+++ packetizer.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import crc8
 
 class Parser():
 
@@ -8,7 +9,7 @@ class Parser():
     def process_data(self, data: bytearray) -> [bytearray]:
 
         class State(Enum):
-            (HEADER, LEN_1, LEN_2, DATA, FOOTER) = range(5)
+            (HEADER, LEN_1, LEN_2, LEN_CRC, DATA, FOOTER) = range(6)
 
         state = State.HEADER
         buf = bytearray()
@@ -27,27 +28,41 @@ class Parser():
                 continue
             if state == State.LEN_2:
                 buf.extend(byte)
-                state = State.DATA
+                state = State.LEN_CRC
                 data_len = int.from_bytes(buf[1:3])
+                continue
+            if state == State.LEN_CRC:
+                buf.extend(byte)
+                hash = crc8.crc8()
+                hash.update(buf[1:3])
+                if hash.digest() == byte:
+                    state = State.DATA
+                else:
+                    state = State.HEADER
+                    self.carryover = bytearray()
+                    buf = bytearray()
+                    data_len = None
                 continue
             if state == State.DATA:
                 buf.extend(byte)
-                if len(buf) - 3 == data_len:
+                if len(buf) - 4 == data_len:
                     state = State.FOOTER
-                if len(buf) - 3 > data_len:
+                if len(buf) - 4 > data_len:
                     raise AssertionError('should not reach this point.  check logic.')
                 continue
             if state == State.FOOTER:
                 buf.extend(byte)
+                carry_hex = str(self.carryover)
+                hex = str(buf)
                 if buf[0] != ord('<') or buf[-1] != ord('>'):
                     raise AssertionError('wrong packet header or footer.  check logic.')
-                if int.from_bytes(buf[1:3]) != len(buf[3:-1]):
+                if int.from_bytes(buf[1:3]) != len(buf[4:-1]):
                     raise AssertionError('packet data length does not match.  check logic.')
-                packets.append(buf[3:-1])
+                packets.append(buf[4:-1])
                 state = State.HEADER
                 self.carryover = bytearray()
                 buf = bytearray()
                 data_len = None
                 continue
-        self.carryover.extend(buf)
+        self.carryover = buf
         return packets
--- test_packetizer.py
+++ test_packetizer.py
@@ -1,83 +1,83 @@
 import unittest
 from packetizer import Parser
 
-x1 = bytearray(b'<\x00\x05hello>')
-y1 = [bytearray(b'hello')]
-x2 = bytearray(b'<\x00\x07<hello>>')
-y2 = [bytearray(b'<hello>')]
-x3 = bytearray(b'<\x00\x0cfirst_packet><\x00\x0dsecond_packet>')
-y3 = [bytearray(b'first_packet'), bytearray(b'second_packet')]
-x4 = bytearray(b'<<\x00\x05hello>')
-y4 = []
-x5 = bytearray(b'xxxx<\x00\x05hello>xxxx')
-y5 = [bytearray(b'hello')]
-x6 = bytearray(b'<\x00\x05hello>>')
-y6 = [bytearray(b'hello')]
-x7a = bytearray(b'<\x00\x0cfirst_pac')
-x7b = bytearray(b'ket><\x00\x0dsecond_packet>')
-y7a = []
-y7b = [bytearray(b'first_packet'), bytearray(b'second_packet')]
-x8a = bytearray(b'<\x00\x0cfirst_packet><\x00\x0dsec')
-x8b = bytearray(b'ond_packet>')
-y8a = [bytearray(b'first_packet')]
-y8b = [bytearray(b'second_packet')]
-x9 = bytearray(b'A><SD><ASD><SA><F><AS>F<SA><D><SA><D><S>G<DS><VD<#R><#><@G#@#RF<>#><V<>@##')
-y9 = []
-x10a = bytearray(b'!@<>R@<#T>G!<F!>#<>!@#D<F><\x00\x0cfirs')
-x10b = bytearray(b't_packe')
-x10c = bytearray(b't>@!<$><@!>$@!><#>!@<FEDD><\x00')
-x10d = bytearray(b'\x0dsecond_packet>@!#@!<>@!<#>!@R#F>D<>...')
-y10a = []
-y10b = []
-y10c = [bytearray(b'first_packet')]
-y10d = [bytearray(b'second_packet')]
-
 class TestPacketizer(unittest.TestCase):
 
     def test_hello(self):
+        x = bytearray(b'<\x00\x05\x1bhello>')
+        y = [bytearray(b'hello')]
         p = Parser()
-        self.assertEqual(y1, p.process_data(x1))
+        self.assertEqual(y, p.process_data(x))
 
     def test_bracket_hello(self):
+        x = bytearray(b'<\x00\x07\x15<hello>>')
+        y = [bytearray(b'<hello>')]
         p = Parser()
-        self.assertEqual(y2, p.process_data(x2))
+        self.assertEqual(y, p.process_data(x))
 
     def test_two_packets(self):
+        x = bytearray(b'<\x00\x0c\x24first_packet><\x00\x0d\x23second_packet>')
+        y = [bytearray(b'first_packet'), bytearray(b'second_packet')]
         p = Parser()
-        self.assertEqual(y3, p.process_data(x3))
+        self.assertEqual(y, p.process_data(x))
 
     def test_double_header(self):
+        x = bytearray(b'<<\x00\x05\x1bhello>')
+        y = []
         p = Parser()
-        self.assertEqual(y4, p.process_data(x4))
+        self.assertEqual(y, p.process_data(x))
 
     def test_nonsense_before_and_after_packet(self):
+        x = bytearray(b'xxxx<\x00\x05\x1bhello>xxxx')
+        y = [bytearray(b'hello')]
         p = Parser()
-        self.assertEqual(y5, p.process_data(x5))
+        self.assertEqual(y, p.process_data(x))
 
     def test_double_footer(self):
+        x = bytearray(b'<\x00\x05\x1bhello>>')
+        y = [bytearray(b'hello')]
         p = Parser()
-        self.assertEqual(y6, p.process_data(x6))
+        self.assertEqual(y, p.process_data(x))
 
     def test_shift_data_to_later(self):
+        x_a = bytearray(b'<\x00\x0c\x24first_pac')
+        x_b = bytearray(b'ket><\x00\x0d\x23second_packet>')
+        y_a = []
+        y_b = [bytearray(b'first_packet'), bytearray(b'second_packet')]
         p = Parser()
-        self.assertEqual(y7a, p.process_data(x7a))
-        self.assertEqual(y7b, p.process_data(x7b))
+        self.assertEqual(y_a, p.process_data(x_a))
+        self.assertEqual(y_b, p.process_data(x_b))
+
 
     def test_shift_data_to_earlier(self):
+        x_a = bytearray(b'<\x00\x0c\x24first_packet><\x00\x0d\x23sec')
+        x_b = bytearray(b'ond_packet>')
+        y_a = [bytearray(b'first_packet')]
+        y_b = [bytearray(b'second_packet')]
         p = Parser()
-        self.assertEqual(y8a, p.process_data(x8a))
-        self.assertEqual(y8b, p.process_data(x8b))
+        self.assertEqual(y_a, p.process_data(x_a))
+        self.assertEqual(y_b, p.process_data(x_b))
 
     def test_rubbish(self):
+        x = bytearray(b'A><SD><ASD><SA><F><AS>F<SA><D><SA><D><S>G<DS><VD<#R><#><@G#@#RF<>#><V<>@##')
+        y = []
         p = Parser()
-        self.assertEqual(y9, p.process_data(x9))
+        self.assertEqual(y, p.process_data(x))
 
     def test_packets_in_rubbish(self):
+        x_a = bytearray(b'!@<>R@<#T>G!<F!>#<>!@#!%(*&#@^(<\x00\x0c\x24firs')
+        x_b = bytearray(b't_packe')
+        x_c = bytearray(b't>@!<$><@!>$@#%!^%&*<F^&#*@%^#<\x00')
+        x_d = bytearray(b'\x0d\x23second_packet>@!#@!<>@!<#>!@R#F>D<>...')
+        y_a = []
+        y_b = []
+        y_c = [bytearray(b'first_packet')]
+        y_d = [bytearray(b'second_packet')]
         p = Parser()
-        self.assertEqual(y10a, p.process_data(x10a))
-        self.assertEqual(y10b, p.process_data(x10b))
-        self.assertEqual(y10c, p.process_data(x10c))
-        self.assertEqual(y10d, p.process_data(x10d))
+        self.assertEqual(y_a, p.process_data(x_a))
+        self.assertEqual(y_b, p.process_data(x_b))
+        self.assertEqual(y_c, p.process_data(x_c))
+        self.assertEqual(y_d, p.process_data(x_d))
 
 if __name__ == '__main__':
     unittest.main()
